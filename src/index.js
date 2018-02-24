6f1ce06d7cc47ae58ec4ea98b0b4de91682366b4
AliaksandraBychkouskaya <aleksandra.bychkovskaya95@gmail.com>
2018-02-24 16:24:59 +0300

    solution for string task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrOfBrackets = [];
+  let flag;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      arrOfBrackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+  
+  do {
+    flag = false;
+    for (let j = 0; j < arrOfBrackets.length; j++) {
+      if (str.indexOf(arrOfBrackets[j]) !== -1) {
+        str = str.replace(arrOfBrackets[j], '');
+        flag = true;
+      }
+    }
+  }
+  while ((str.length !== 0) && (flag !== false));
+
+  return flag;
 }
